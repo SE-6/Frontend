5d333e61306a2e3ca8bd20d65230a5807868d751
Garrett Lander <garrett.jlander@gmail.com>
2026-03-24 09:41:10 +0100

    higher order functions lecture

---
+++ 09-js-fundamentals/12-higher-order-functions-lecture/index.js
@@ -0,0 +1,52 @@
+const print = (content) => console.log(content);
+
+// print('Ah, like Python!');
+
+const printArray = (array) => {
+	for (let i = 0; i < array.length; i++) {
+		print(array[i]);
+	}
+};
+
+const avatarChars = [
+	{ name: 'Aang', nation: 'Air Nation', isBender: true },
+	{ name: 'Katara', nation: 'Water Tribe', isBender: true },
+	{ name: 'Sokka', nation: 'Water Tribe', isBender: false }
+];
+
+// printArray(avatarChars);
+
+const sayHello = (char) =>
+	console.log(`Hi, I'm ${char.name}, and I'm from the ${char.nation}!`);
+
+const sayHelloLoop = (array) => {
+	for (let i = 0; i < array.length; i++) {
+		sayHello(array[i]);
+	}
+};
+
+// sayHelloLoop(avatarChars);
+
+const higherOrderArrayFunc = (array, callbackFunc) => {
+	for (let i = 0; i < array.length; i++) {
+		callbackFunc(array[i]);
+	}
+};
+
+// higherOrderArrayFunc(avatarChars, print);
+// higherOrderArrayFunc(avatarChars, sayHello);
+
+higherOrderArrayFunc(avatarChars, (obj) => {
+	obj.isBender
+		? console.log("I'm a bender!")
+		: console.log("I can't bend an element, but I've got my wits!");
+});
+
+avatarChars.forEach((obj) => {
+	obj.isBender
+		? console.log("I'm a bender!")
+		: console.log("I can't bend an element, but I've got my wits!");
+});
+
+avatarChars.forEach((c) => print(c));
+avatarChars.forEach((char) => sayHello(char));
